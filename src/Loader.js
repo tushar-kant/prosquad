5ece32b236c4a2e6a04f9543675b1d96807aa2df
[tushar-kant] <[ece.190410029@silicon.ac.in]>
2023-03-20 17:24:32 +0530

    loader ,login,registration

---
+++ src/Loader.js
@@ -0,0 +1,23 @@
+import React from "react";
+// import { RotatingCircleLoader   } from "react-loaders-kit";
+// import Typed from "react-typed";
+import './Loader.css'
+
+function Loader() {
+
+    //   const loaderProps = {
+    //     loading: true,
+    //     size:300,
+    //     duration: 2,
+    //     colors: ["blue", "green", "red"],
+    //   };
+
+    return (
+            <div id="preloader">
+            <div id="loader"></div>
+          </div>
+       
+    );
+}
+
+export default Loader;
