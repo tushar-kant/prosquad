954c7328e21c05f4aa4b22a019753fbf5c60c170
ankush9054 <dasankush1511@gmail.com>
2023-03-21 22:14:16 +0530

    done with user auth in backend

---
+++ backend/config/allowedOrigin.js
@@ -0,0 +1,7 @@
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://www.ezioauditore.com',
+    'https://www.ezioshop.com'
+]
+
+module.exports = allowedOrigins
--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,52 @@
-const express = require( 'express' );
-const tours = require( './data/tours' )
-const dotenv = require( 'dotenv' )
-
+require( 'dotenv' ).config()
+const express = require( 'express' )
 const app = express()
-dotenv.config();
+const path = require( 'path' )
+const { logger } = require( './middleware/logger' )
+const errorHandler = require( './middleware/errorHandler' )
+const cookieParser = require( 'cookie-parser' )
+const cors = require( 'cors' )
+const corsOptions = require( './config/corsOptions' )
+const connectDB = require( './config/db.Conn' )
+const mongoose = require( 'mongoose' )
+const { logEvents } = require( './middleware/logger' )
 
+const PORT = process.env.PORT || 3500
 
-app.get( "/", ( req, res ) => {
-    res.send( "API is Running" );
-} );
+console.log( process.env.NODE_ENV );
 
-app.get( '/api/tours', ( req, res ) => {
-    res.json( tours )
+connectDB()
+
+app.use( logger )
+app.use( cors( corsOptions ) )
+
+app.use( express.json() )
+
+app.use( cookieParser() )
+
+app.use( '/', express.static( path.join( __dirname, 'public' ) ) )
+
+app.use( '/', require( './routes/root' ) )
+app.use( '/users', require( './routes/userRoutes' ) )
+
+app.all( '*', ( req, res ) => {
+    res.status( 404 )
+    if ( req.accepts( 'html' ) ) {
+        res.sendFile( path.join( __dirname, 'views', '404.html' ) )
+    } else if ( req.accepts( 'josn' ) ) {
+        res.json( { message: '404 Not Found' } )
+    } else {
+        res.type( 'txt' ).send( '404 Not Found' )
+    }
+} )
+app.use( errorHandler )
+
+mongoose.connection.once( 'open', () => {
+    console.log( 'Connectec to mongoDB' )
+    app.listen( PORT, () => console.log( `server is running at port ${PORT}` ) )
 } )
 
-app.get( "/api/tours/:id", ( req, res ) => {
-    const tour = tours.find( ( t ) => t._id === req.params.id )
-    res.send( tour )
-} );
-
-const PORT = process.env.PORT || 5000;
-
-app.listen( 5000, console.log( `Server started on PORT ${PORT}` ) )
+mongoose.connection.on( 'error', err => {
+    console.log( err );
+    logEvents( `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log' )
+} )
