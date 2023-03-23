f8875081ac8a662042c39cd22fbac7894f5d96fc
ankush9054 <dasankush1511@gmail.com>
2023-03-23 22:34:52 +0530

    fixed minor bugs

--- src/components/LoginRegister/Register.js
+++ src/components/LoginRegister/Register.js
@@ -1,40 +1,45 @@
-import React from 'react'
 import './Login.css';
+import { useState } from 'react';
 
 
 const Register = () => {
-    return (
-        <>
-        <div class="login-box">
-  <h2>Register</h2>
-  <form>
-  <div class="user-box">
-      <input type="email" name="" required=""/>
-      <label>EmailID</label>
+  const [ username, setUsername ] = useState( '' );
+  const [ email, setEmail ] = useState( '' );
+  const [ password, setPassword ] = useState( '' );
+  const [ confirmPassword, setConfirmPassword ] = useState( '' );
+
+  return (
+    <div>
+      <div class="login-box">
+        <h2>Register</h2>
+        <form>
+          <div class="user-box">
+            <input type="email" name="" required="" />
+            <label>EmailID</label>
+          </div>
+          <div class="user-box">
+            <input type="text" name="" required="" />
+            <label>Username</label>
+          </div>
+          <div class="user-box">
+            <input type="password" name="" required="" />
+            <label>Password</label>
+          </div>
+          <div class="user-box">
+            <input type="password" name="" required="" />
+            <label> confirm Password</label>
+          </div>
+          <a href="#">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            SignUP
+          </a>
+        </form>
+      </div>
     </div>
-    <div class="user-box">
-      <input type="text" name="" required=""/>
-      <label>Username</label>
-    </div>
-    <div class="user-box">
-      <input type="password" name="" required=""/>
-      <label>Password</label>
-    </div>
-    <div class="user-box">
-      <input type="password" name="" required=""/>
-      <label> confirm Password</label>
-    </div>
-    <a href="#">
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      SignUP
-    </a>
-  </form>
-</div>
-        </>
-    )
+  )
 }
 
 export default Register
